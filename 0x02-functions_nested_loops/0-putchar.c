9fa4582b42d3d8df74217ff617c3738fdc6538b8
Mohau Maboe <mamaboe@gmail.com>
2023-02-21 08:47:36 -0800

    C - Functions, nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,21 @@
-#include <stdio.h>
+#include "main.h"
+/**
+ * main - prints _putchar
+ *
+ * Return: Always 0 in success
+ */
+int main(void)
+{
+	_putchar(95); /*95 is asci for _*/
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-int main(void) {
-    printf("_putchar\n");
-    return 0;
+	return (0);
 }
 
